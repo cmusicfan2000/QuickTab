87f662d4e3fff02dbfe59f77799ae1cd8449b08e
Gregory Loomis <cmusicfan2000@gmail.com>
2016-11-10 15:53:04 -0600

    Added Compositions view model and compositions view. Currently displays a dummy set of lyrics to the user.

---
+++ QuickTab.ViewModels/CompositionViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace QuickTab.ViewModels
+{
+    public class CompositionViewModel
+    {
+        public string Lyrics
+        {
+            get
+            {
+                return "This is the song that never ends. It's going on and on my friend.";
+            }
+        }
+    }
+}
