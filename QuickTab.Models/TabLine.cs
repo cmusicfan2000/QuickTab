5077e7c0d546d5fdf97092dc192980151702223c
Gregory Loomis <cmusicfan2000@gmail.com>
2016-11-19 10:09:50 -0600

    Created a Tab viewmodel

--- QuickTab.Models/TabLine.cs
+++ QuickTab.Models/TabLine.cs
@@ -1,22 +1,53 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using QuickTab.Generics;
 
 namespace QuickTab.Models
 {
     /// <summary>
     /// Represents a single line of tabliture which is part of a tab
     /// </summary>
-    public class TabLine
+    public class TabLine : ICompositionLine
     {
         #region [ Properties ]
         /// <summary>
         /// A collection of notes that are on this line of tabliture
         /// </summary>
         private List<Note> _notes = new List<Note>();
-
+\
         /// <summary>
         /// Used to arrange strings of tabliture within a tab
         /// </summary>
-        public int LineOrder { get; private set; }
+        public int LineNumber { get; private set; }
+
+        /// <summary>
+        /// Returns a string representation of this line of tabliture
+        /// </summary>
+        public string Content
+        {
+            get
+            {
+                string c = string.Empty;
+
+                foreach(Note n in _notes)
+                {
+                    c += n.Fret.ToString().PadLeft(n.PrecedingSpaces);
+                }
+
+                return c;
+            }
+        }
+        #endregion
+
+        #region [ Constructors ]
+        /// <summary>
+        /// Creates an empty line of tabliture that is arranged with other tab lines
+        /// </summary>
+        /// <param name="order"></param>
+        public TabLine(int order)
+        {
+            LineNumber = order;
+        }
         #endregion
 
         #region [ Methods ]
--- QuickTab.ViewModels/TabViewModel.cs
+++ QuickTab.ViewModels/TabViewModel.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using QuickTab.Models;
 using QuickTab.Generics;
 
@@ -13,22 +10,97 @@ namespace QuickTab.ViewModels
     /// </summary>
     public class TabViewModel : ViewModelBase, ICompositionLine
     {
+        #region [ Properties ]
+        /// <summary>
+        /// A list of lines of tabliture
+        /// </summary>
+        private List<TabLine> _models;
+
+        /// <summary>
+        /// The tab in the form of a string
+        /// </summary>
         public string Content
         {
             get
             {
-                throw new NotImplementedException();
+                string tabContent = string.Empty;
+                foreach(TabLine tl in _models)
+                {
+                    tabContent += tl.Content + "\n";
+                }
+                return tabContent;
             }
         }
 
-        public int LineNumber
+        /// <summary>
+        /// Used to sort this tab with other tabs in the composition
+        /// </summary>
+        public int LineNumber { get; private set; }
+        #endregion
+
+        #region [ Constructors ]
+        /// <summary>
+        /// Creates an empty tab at the given line number
+        /// </summary>
+        /// <param name="lineNumber">Orders this tab with other composition items</param>
+        public TabViewModel(int lineNumber)
         {
-            get
-            {
-                throw new NotImplementedException();
-            }
+            LineNumber = LineNumber;
         }
+        #endregion
+        
+        #region [ Methods ]
+        /// <summary>
+        /// Add a line of tabliture to this tab
+        /// </summary>
+        /// <param name="lineContent">The content of the tab line</param>
+        public void AddLineOfTabliture(string lineContent)
+        {
+            int fretNumber;
+            int preceedingSpaces = 0;
+            int noteCount = 0;
+            string fret = string.Empty;
+            TabLine newLine = new TabLine(_models.Count + 1);
 
+            foreach(char c in lineContent)
+            {
+                // IF this character is a space character
+                // - Process preceeding notes if applicable
+                // - Increment the number of preceeding spaces for the next note
+                // ELSE IF this character is a number
+                // - add the number to the fret string (this handles frets > 9)
+                // - Reset the number of preceeding spaces
+                // ENDIF
+                if(c == '-')
+                {
+                    // IF a fret has been recorded
+                    // - Add a new note to the line for it
+                    // ENDIF
+                    if(fret != string.Empty)
+                    {
+                        // Convert the fret into an integer
+                        int.TryParse(fret, out fretNumber);
 
+                        // Add the new note to the tab line
+                        newLine.AddNote(fretNumber, preceedingSpaces, noteCount++);
+
+                        // Reset the fret number
+                        fret = string.Empty;
+                    }
+
+                    // Increment the number of preceeding spaces
+                    preceedingSpaces++;
+                }
+                else if(char.IsNumber(c))
+                {
+                    fret += c;
+                    preceedingSpaces = 0;
+                }
+            }
+
+            // Add the line to the models
+            _models.Add(newLine);
+        }
+        #endregion
     }
 }
