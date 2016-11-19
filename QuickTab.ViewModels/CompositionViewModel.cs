f57beb6cb8718b2c161b2a9342a1459fd5e9aecf
Gregory Loomis <cmusicfan2000@gmail.com>
2016-11-19 10:19:53 -0600

    Added a tab to the composition model

--- QuickTab.ViewModels/CompositionViewModel.cs
+++ QuickTab.ViewModels/CompositionViewModel.cs
@@ -42,12 +42,21 @@ namespace QuickTab.ViewModels
             chordLineTwo.AddChord("Am", 4, 9);
             chordLineTwo.AddChord("C", 5, 5);
 
+            TabViewModel testTab = new TabViewModel(7);
+            testTab.AddLineOfTabliture("e--------------------------------------");
+            testTab.AddLineOfTabliture("B---3---3---3---3---3-----3---5-5p3----");
+            testTab.AddLineOfTabliture("G-----2-------2-------2h4---2-------2--");
+            testTab.AddLineOfTabliture("D-0-------4-------5--------------------");
+            testTab.AddLineOfTabliture("A--------------------------------------");
+            testTab.AddLineOfTabliture("E--------------------------------------");
+
             Lines.Add(chordLineOne);
             Lines.Add(new LyricViewModel(2, " I believe in night, I believe in day"));
             Lines.Add(chordLineTwo);
             Lines.Add(new LyricViewModel(4, " and I believe there's a light coming back around again"));
             Lines.Add(new LyricViewModel(5, " And I believe you're right cause I believe there's a way"));
             Lines.Add(new LyricViewModel(6, " And is believe is might have some catching up to do"));
+            
         }
         #endregion
     }
